b8d0808b9b5e9236591cb6b3ab917cb578f33e43
root <root@t480>
2026-01-28 16:05:13 -0300

    Removal of unused parameter overide (this is already managed by assigning the alternative parameter set) and removal of a not used plt.plot() command

--- generate_figure3B.py
+++ generate_figure3B.py
@@ -24,7 +24,6 @@ for f in f_vals:
         post_times = [],
         parameters = extracellular_stimulation(),
         T = model_runtime*second,
-        params_override = {"s_value": 10*mV}
     )
     W_inf.append(M.Wnorm[0][-1])
 
@@ -36,6 +35,5 @@ plt.ylim(0, 4)
 plt.axhline(y=1.0,linestyle ="--",color = "#888A85")
 plt.xlabel("Frequency (Hz)")
 plt.ylabel(r"$W_{final} / W_0$", rotation = 90 )
-plt.plot(f_vals, W_inf)
 plt.savefig("produced_figures/3B_without_postsynaptic_contributions.png")
 plt.close("all")
